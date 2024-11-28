163d172be8f940ed295423d73cbf82ccd8088131
Sandra <sandra0924@protonmail.com>
2024-11-28 21:21:09 +0100

    added basic html and began writing script

---
+++ script.js
@@ -0,0 +1,60 @@
+/* Pokémon API = https://pokeapi.co/api/v2/{endpoint}/ */
+
+const pokedex = document.getElementById('pokedex'); // hitta pokédex-diven
+
+fetch('https://pokeapi.co/api/v2/pokemon?limit=151') // väljer endpoint pokémon och begränsar mig till första generationen som omfattar de första 150, från bulbasaur (1) till mew (151)
+.then(response => response.json())
+.then(data => {
+    fetchAllPokemon = data.results;
+    console.log(data.results); // såhär kan jag kolla vad api:n innehåller via dev tools
+
+    const pokemons = data.results; // här sparas alla pokémon i en array
+
+    // här loopar jag alla pokémon från arrayen
+    pokemons.forEach(pokemon => { 
+
+        const pokediv = document.createElement('div'); // skapar ett div-element för varje pokémon
+        pokediv.setAttribute('id', pokemon.name); // ger diven pokemonens namn som id
+        pokediv.classList.add('poke-div'); // ger diven en class så man kan appenda mer data i senare
+        
+        const btnOpen = document.getElementById('open');
+        const btnClose = document.getElementById('close');
+        
+        btnOpen.addEventListener('click', () => { 
+            pokedex.appendChild(pokediv); // appendar div-elementen till pokédex-div (öppna)
+        });        
+        btnClose.addEventListener('click', () => { 
+            pokedex.removeChild(pokediv); // tar bort div-elementen från pokédex-div (stäng)
+        });
+        
+        const pokemonURL = `${pokemon.url}`; // url till pokémonens egna api (med fler egenskaper)
+
+        const getPokemon = async (pokemon) => {
+            try {
+                const response = await fetch(pokemonURL);
+                const pokemonData = await response.json();
+                console.log(pokemonData); // så jag kan kolla api:ns innehåll via dev tools
+
+                const poke_data_div = document.createElement('div'); // skapa ett div-element
+                poke_data_div.classList.add('poke-data-div'); // ge div en class
+                poke_data_div.innerHTML = `
+                <img src="${pokemonData.sprites.front_default}" />
+                <img src="${pokemonData.sprites.back_default}" />
+                <h2>${pokemonData.name}</h2>
+                <p>no ${pokemonData.id}</p>
+                `;
+
+                pokediv.appendChild(poke_data_div);
+
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        getPokemon();
+        
+
+    }); //close foreach
+}) .catch(error => {
+    console.error('Error fetching data:', error);
+});
